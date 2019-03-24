e63cb675d9627e1f997da18870414d763d428687
gkaran555 <gkaran555@gmail.com>
2019-03-24 12:37:06 +0100

    dva

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-      ? '/goranb/'
-      : '/'
-  }
+// module.exports = {
+//     baseUrl: process.env.NODE_ENV === 'production'
+//       ? '/goranb/'
+//       : '/'
+//   }
